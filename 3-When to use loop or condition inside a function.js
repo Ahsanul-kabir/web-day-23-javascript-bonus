b780bbd48a1bbee99c9265e9fd634a0060b194f3
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-01-23 15:51:00 +0600

    3-When to use loop or condition inside a functionadded

---
+++ 3-When to use loop or condition inside a function.js
@@ -0,0 +1,16 @@
+// here i apply demo
+
+function addNum(number1, number2, number3) {
+    // no need for loop
+}
+
+function addNum(numbersASarray) {
+    // need for loop
+}
+
+function addNum(numbersASarray) {
+    // need for loop
+    if (numbersASarray[i] > 0) { // apply condition
+        //then execute for loop
+    }
+}
